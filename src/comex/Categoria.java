9acb0a687471a97003e4f15e657bf1924e48214a
unknown <alexandre.silva@brxretail.com>
2022-09-23 17:44:12 -0300

    Update Categoria

--- src/comex/Categoria.java
+++ src/comex/Categoria.java
@@ -1,5 +1,48 @@
 package comex;
 
 public class Categoria {
+	private static long count = 0;
+	private long id;
+	private String nome;
+	private boolean status = true;
+	
+	
+	
+	
+	public Categoria() {
+		id = ++count;
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
 
+	public String recuperaStatus() {
+		if (status == true) {
+			
+			return "ativa";
+			
+			
+		}else {
+			return "inativa";
+		}
+	}
+	
+	
 }
