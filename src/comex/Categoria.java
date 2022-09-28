6288008f7401aad93cfdd304e8aa3eecb6f55df6
unknown <alexandre.silva@brxretail.com>
2022-09-28 16:13:25 -0300

    Exercicio 1 - Validações ao criar uma Categoria

--- src/comex/Categoria.java
+++ src/comex/Categoria.java
@@ -4,20 +4,41 @@ public class Categoria {
 	private static long count = 0;
 	private long id;
 	private String nome;
-	private boolean status = true;
+	private StatusDaCategoria status;
 	
 	
 	
 	
-	public Categoria() {
-		id = ++count;
+
+	public Categoria(long id, String nome, StatusDaCategoria status ) {
+		//id = ++count;
+		this.status = status;
+		this.id = id;
+		this.nome = nome;
+		if(id <= 0) {
+			throw new IllegalArgumentException("numero invalido");
+		}
+		if(nome.length() <= 3) {
+			
+			throw new IllegalArgumentException("O nome ta zoado");
+			
+		}
+		if((status != StatusDaCategoria.ATIVADO)&&(status != StatusDaCategoria.DESATIVADO)) {
+			
+			throw new IllegalArgumentException("O status n ta certo irmão");
+		}
 		
+		
+	
+		//id = ++count;
+	
 	}
 	
 	public long getId() {
 		return id;
 	}
 	public void setId(long id) {
+		
 		this.id = id;
 	}
 	public String getNome() {
@@ -26,23 +47,22 @@ public class Categoria {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public boolean isStatus() {
+
+	public StatusDaCategoria getStatus() {
 		return status;
 	}
-	public void setStatus(boolean status) {
+
+	public void setStatus(StatusDaCategoria status) {
 		this.status = status;
 	}
+	//public boolean isStatus() {
+	//	return status;
+	//}
+	//public void setStatus(boolean status) {
+	//	this.status = status;
+	//}
 
-	public String recuperaStatus() {
-		if (status == true) {
-			
-			return "ativa";
-			
-			
-		}else {
-			return "inativa";
-		}
-	}
+	
 	
 	
 }
--- src/comex/MainCategoria.java
+++ src/comex/MainCategoria.java
@@ -3,29 +3,29 @@ package comex;
 public class MainCategoria {
 
 	public static void main(String[] args) {
+	
+		Categoria categoria1 = new Categoria(3, "INFORMATIC",StatusDaCategoria.DESATIVADO );
+	
+	
 		
-		Categoria categoria1 = new Categoria();
+		Categoria categoria2 = new Categoria(1, "Moveis",StatusDaCategoria.ATIVADO);
+	
 		
-		categoria1.setNome("informatica ");
-		categoria1.setStatus(true);
 		
-		Categoria categoria2 = new Categoria();
+		Categoria categoria3 = new Categoria(2, "livros",StatusDaCategoria.ATIVADO );
+	
 		
-		categoria2.setNome("Moveis ");
-		categoria2.setStatus(false);
 		
-		Categoria categoria3 = new Categoria();
 		
-		categoria3.setNome("livros ");
-		categoria3.setStatus(true);
+		
 		
 		
 		
 	
 		
-		System.out.println("CATEGORIA " + categoria1.getNome() + "(" + categoria1.getId()+" - " + categoria1.recuperaStatus() + ")");
-		System.out.println("CATEGORIA " + categoria2.getNome() + "(" + categoria2.getId()+" - " + categoria2.recuperaStatus() + ")");
-		System.out.println("CATEGORIA " + categoria3.getNome() + "(" + categoria3.getId()+" - " + categoria3.recuperaStatus() + ")");
+		System.out.println("CATEGORIA " + categoria1.getNome() + "(" + categoria1.getId()+" - " + categoria1.getStatus() + ")");
+		System.out.println("CATEGORIA " + categoria2.getNome() + "(" + categoria2.getId()+" - " + categoria2.getStatus() + ")");
+		System.out.println("CATEGORIA " + categoria3.getNome() + "(" + categoria3.getId()+" - " + categoria3.getStatus() + ")");
 		
 		
 		
